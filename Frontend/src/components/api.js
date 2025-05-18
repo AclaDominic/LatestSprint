83a325822528e29e7973ea73e2a47db81569c0af
Acla <acladominic10@gmail.com>
2025-05-18 13:35:58 +0800

    Link update

--- Frontend/src/components/api.js
+++ Frontend/src/components/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://bd9d-149-30-138-24.ngrok-free.app/api';
+export const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
